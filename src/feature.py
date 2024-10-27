c8727e9de4891cf78f2b485bc53701b958c148ae
Smveer <singh.manveer93@icloud.com>
2024-10-27 21:02:28 +0000

    TD1 first commit

--- src/feature.py
+++ src/feature.py
@@ -1,7 +1,20 @@
-def make_features(df):
+from sklearn.feature_extraction.text import CountVectorizer
+from data import make_dataset
+
+
+def make_features(df, vectorizer = None):
     y = df["is_comic"]
+    
+    if vectorizer is None:
+        vectorizer = CountVectorizer(analyzer='word')
+        X = vectorizer.fit_transform(df["video_name"])
+    else:
+        X = vectorizer.transform(df["video_name"])
+    
+    return X, y, vectorizer
 
-    # Fake X: constant equal to 0
-    X = [[0]] * len(y)
-
-    return X, y
+ def preprocess_text(text):
+    stemmer = PorterStemmer()
+    stop_words = set(stopwords.words("french"))
+    words = text.lower().split()
+    return " ".join([stemmer.stem(word) for word in words if word not in stop_words])
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import click
 import joblib
-
+import pandas as pd
 from data import make_dataset
 from feature import make_features
 from models import make_model
@@ -15,21 +15,36 @@ def cli():
 @click.option("--model_dump_filename", default="models/dump.json", help="File to dump model")
 def train(input_filename, model_dump_filename):
     df = make_dataset(input_filename)
-    X, y = make_features(df)
+    X, y, vectorizer = make_features(df)
 
     model = make_model()
     model.fit(X, y)
 
-    return joblib.dump(model, model_dump_filename)
+    joblib.dump(model, model_dump_filename)
+    joblib.dump(vectorizer, "models/vectorizer.json")
 
+    
 
 @click.command()
 @click.option("--input_filename", default="data/raw/train.csv", help="File training data")
 @click.option("--model_dump_filename", default="models/dump.json", help="File to dump model")
 @click.option("--output_filename", default="data/processed/prediction.csv", help="Output file for predictions")
 def predict(input_filename, model_dump_filename, output_filename):
+    df = make_dataset(input_filename)
+    
     model = joblib.load(model_dump_filename)
-    pass
+    vectorizer = joblib.load("models/vectorizer.json")
+    X, y, vectorizer = make_features(df, vectorizer = vectorizer)
+
+    
+    predictions = model.predict(X)
+    
+    results_df = pd.DataFrame({
+        "video_name": X,
+        "is_comic_pred": predictions
+    }).to_csv(output_filename, index=False)
+
+    return model.predict(X)
 
 
 @click.command()
@@ -39,7 +54,7 @@ def evaluate(input_filename):
     df = make_dataset(input_filename)
 
     # Make features (tokenization, lowercase, stopwords, stemming...)
-    X, y = make_features(df)
+    X, y, vectorizer = make_features(df)
 
     # Object with .fit, .predict methods
     model = make_model()
